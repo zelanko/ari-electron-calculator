d217b47957a9b3037a5c83eb7b2a45c38ac527b3
Ari Zelanko <zelanko@yahoo.com>
2025-11-16 15:53:47 -0500

    basic electron app.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+  const windowSpec = {
+    width: 800,
+    height: 600
+  };
+
+  const win = new BrowserWindow(windowSpec);
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
