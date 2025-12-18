38b4a1a37662c41a5a3e54b80e4124b8dd51757d
Ari Zelanko <zelanko@yahoo.com>
2025-12-18 10:37:41 -0500

    Final form of the little browser app.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import { app, BrowserWindow } from 'electron';
 
 const createWindow = () => {
-  const windowSpec = {
+  const win = new BrowserWindow({
     width: 800,
     height: 600
-  };
-
-  const win = new BrowserWindow(windowSpec);
+  });
 
   win.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
